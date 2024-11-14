6b366dc5a7564f0a04ec48afb5e5b318651cb780
Prince Isaac Pantino <arispantino@gmail.com>
2024-11-14 12:54:11 +0800

    feat(models): added preferences model

---
+++ src/models/workout-preference.model.ts
@@ -0,0 +1,36 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/database";
+
+const preferences = sequelize.define('Preferences', {
+    preference_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    goal_type: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    with_gym: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false
+    },
+    workout_days: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    intensity: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+}, {
+    underscored: true,
+    timestamps: true
+});
+
+export default preferences;
