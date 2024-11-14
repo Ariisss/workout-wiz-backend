ffe6b707bed2c24e72c01f1a6221da2317b6c993
Prince Isaac Pantino <arispantino@gmail.com>
2024-11-14 15:34:53 +0800

    feat(models): added exercise log model

---
+++ src/models/exercise-log.model.ts
@@ -0,0 +1,38 @@
+import sequelize from "../config/database";
+import { DataTypes } from "sequelize";
+
+const ExerciseLog = sequelize.define('ExerciseLog', {
+    log_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+    },
+    user_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    exercise_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    date: {
+        type: DataTypes.DATE,
+        allowNull: false,
+        defaultValue: DataTypes.NOW
+    },
+    duration_mins: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    calories_burned: {
+        type: DataTypes.FLOAT,
+        allowNull: false
+    }
+}, {
+    tableName: 'exercise_logs',
+    underscored: true,
+    timestamps: true
+});
+
+export default ExerciseLog;
