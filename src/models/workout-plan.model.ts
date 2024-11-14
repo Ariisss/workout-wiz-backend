9ec0b8fb6674b9653003073d731cc0d403af3326
Prince Isaac Pantino <arispantino@gmail.com>
2024-11-14 15:25:20 +0800

    feat(models): added workout-plan model

---
+++ src/models/workout-plan.model.ts
@@ -0,0 +1,49 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/database";
+
+const WorkoutPlan = sequelize.define(
+    'WorkoutPlan',
+    {
+        plan_id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false
+        },
+        user_id: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        plan_name: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        description: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        },
+        goal: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        duration_weeks: {
+            type: DataTypes.INTEGER,
+            allowNull: false
+        },
+        intensity: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        is_active: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: true
+        }
+    }, {
+        tableName: "workout_plans",
+        underscored: true,
+        timestamps: true
+    }
+);
+
+export default WorkoutPlan;
