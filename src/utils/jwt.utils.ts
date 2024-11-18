43ec7e80563ac0fcceef1b7d7f0ccfd563cfb2b3
Prince Isaac Pantino <arispantino@gmail.com>
2024-11-18 15:12:36 +0800

    feat: implemented jwt token verification

--- src/utils/jwt.utils.ts
+++ src/utils/jwt.utils.ts
@@ -4,3 +4,7 @@ import env from "../config/environment";
 export const generateToken = (payload: object): string => {
     return jwt.sign(payload, env.JWT_SECRET, { expiresIn: '1h' });
 };
+
+export const verifyToken = (token: string): any => {
+    return jwt.verify(token, env.JWT_SECRET);
+};
