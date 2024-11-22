3b821cfbd928ee49f09646ae64d7c45ae9bbbdba
Prince Isaac Pantino <arispantino@gmail.com>
2024-11-22 15:33:30 +0800

    feat: added get, update user and change password services

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,4 +1,6 @@
 import { User } from "../models";
+import { UserType } from "../types/types";
+import { hashPassword } from "../utils/password.utils";
 
 export const getUser = async (userId: number) => {
     const user = await User.findByPk(userId);
@@ -7,9 +9,26 @@ export const getUser = async (userId: number) => {
 
 // update user
 // takes userId and updated fields, returns updated user
+export const updateUser = async (userId: number, updatedFields: Partial<UserType>) => {
+    const user = await User.findByPk(userId);
+    if (!user) {
+        throw new Error("User not found");
+    }
+    return await user.update(updatedFields);
+}
 
 // change password
 // takes userId and new password, validates and hashes new password
+export const changePassword = async (userId: number, newPassword: string) => {
+    const user = await User.findByPk(userId);
+    if (!user) {
+        throw new Error("User not found");
+    }
+
+    const hashedPassword = hashPassword(newPassword);
+
+    return await user.update({ password: hashedPassword });
+}
 
 // delete user
 // takes userId and performs cleanup/deletion
