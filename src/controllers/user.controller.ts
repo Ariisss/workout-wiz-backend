debe68bb7ea8912be544effdc0a394226371819d
Prince Isaac Pantino <arispantino@gmail.com>
2024-11-23 22:54:57 +0800

    feat: added remove user function

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -65,5 +65,32 @@ export const updatePassword = async (req: Request, res: Response) => {
     }
 }
 
+export const removeUser = async (req: Request, res: Response) => {
+    try{
+        const { userId } = req.params;
+
+        if (!userId) {
+            return res.status(400).json({ error: 'id is required' });
+        }
+
+        const parsedUserId = parseInt(userId);
+        if (isNaN(parsedUserId)) {
+            return res.status(400).json({ error: 'invalid id format' });
+        }
+
+        const user = await deleteUser(parsedUserId);
+
+        return res.status(200).json({
+            success: true,
+            message: 'User deleted successfully',
+        });
+
+    }catch(error){
+        if (error instanceof Error) {
+            return res.status(400).json({ error: error.message });
+        }
+        return res.status(500).json({ error: 'Internal server error' });
+    }
+}
 
 
