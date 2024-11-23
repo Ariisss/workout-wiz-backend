4b4454b435c0cd29f2dca6d59a88fff1adc1435c
Prince Isaac Pantino <arispantino@gmail.com>
2024-11-23 22:43:54 +0800

    feat: added change password functionality

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from "express";
-import { updateUser, changePassword, deleteUser, updateHealthMetrics } from "../services/user.service";
+import { update, changePassword, deleteUser, updateHealthMetrics } from "../services/user.service";
 
-export const updateUserController = async (req: Request, res: Response) => {
+export const updateUser = async (req: Request, res: Response) => {
     try {
         const { userId } = req.params;
         const updatedFields = req.body;
@@ -15,7 +15,7 @@ export const updateUserController = async (req: Request, res: Response) => {
             return res.status(400).json({ error: 'invalid id format' });
         }
 
-        const user = await updateUser(parsedUserId, updatedFields);
+        const user = await update(parsedUserId, updatedFields);
         
         if (!user) {
             return res.status(404).json({ error: 'User not found' });
@@ -29,3 +29,35 @@ export const updateUserController = async (req: Request, res: Response) => {
         return res.status(500).json({ error: 'Internal server error' });
     }
 }
+
+export const updatePassword = async (req: Request, res: Response) => {
+    try{
+        const { userId } = req.params;
+        const { newPassword } = req.body;
+
+        if (!userId) {
+            return res.status(400).json({ error: 'id is required' });
+        }
+
+        const parsedUserId = parseInt(userId);
+        if (isNaN(parsedUserId)) {
+            return res.status(400).json({ error: 'invalid id format' });
+        }
+
+        const user = await changePassword(parsedUserId, newPassword);
+
+        if (!user) {
+            return res.status(404).json({ error: 'User not found' });
+        }
+
+        return res.status(200).json(user);
+    }catch(error){
+        if (error instanceof Error) {
+            return res.status(400).json({ error: error.message });
+        }
+        return res.status(500).json({ error: 'Internal server error' });
+    }
+}
+
+
+
