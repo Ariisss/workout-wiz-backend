6664fd3c3964a471693a25fbb33fac4dfae60962
Prince Isaac Pantino <arispantino@gmail.com>
2024-11-23 16:38:00 +0800

    feat: added validation middleware function

--- src/middleware/validate.middleware.ts
+++ src/middleware/validate.middleware.ts
@@ -1,1 +1,38 @@
-// validator middleware
+import { Request, Response, NextFunction } from "express"
+import Joi from "joi"
+import { Schema } from "joi";
+
+interface ValidateOptions {
+    body?: Schema;
+    query?: Schema;
+    params?: Schema;
+}
+
+export function validate(schemas: ValidateOptions) {
+    return async (req: Request, res: Response, next: NextFunction) => {
+      try {
+        if (schemas.body) {
+          req.body = await schemas.body.validateAsync(req.body, { abortEarly: false });
+        }
+  
+        if (schemas.query) {
+          req.query = await schemas.query.validateAsync(req.query, { abortEarly: false });
+        }
+  
+        if (schemas.params) {
+          req.params = await schemas.params.validateAsync(req.params, { abortEarly: false });
+        }
+  
+        return next();
+      } catch (error) {
+        return res.status(400).json({
+          status: 'error',
+          message: 'Validation failed',
+          errors: (error as Joi.ValidationError).details?.map(detail => ({
+            field: detail.path.join('.'), 
+            message: detail.message 
+          }))
+        });
+      }
+    };
+}
