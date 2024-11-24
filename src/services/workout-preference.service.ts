241c0bbad52c246145c70231c8861f8c61d39371
Prince Isaac Pantino <arispantino@gmail.com>
2024-11-24 13:03:14 +0800

    feat: added createWorkoutPref function

--- src/services/workout-preference.service.ts
+++ src/services/workout-preference.service.ts
@@ -1,5 +1,7 @@
 import { Preferences } from "../models";
 import { WorkoutPreferenceType } from "../types/types";
 
-
+export const createWorkoutPreference = async (workoutPreference: Omit<WorkoutPreferenceType, 'preference_id'>) => {
+    return await Preferences.create(workoutPreference);
+}
 
