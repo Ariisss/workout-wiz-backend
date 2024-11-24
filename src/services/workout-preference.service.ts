1fb9df54874c34097336e094eafd107e3766894b
Prince Isaac Pantino <arispantino@gmail.com>
2024-11-24 13:09:23 +0800

    feat: added CRUD operations on workout preference

--- src/services/workout-preference.service.ts
+++ src/services/workout-preference.service.ts
@@ -5,3 +5,16 @@ export const createWorkoutPreference = async (workoutPreference: Omit<WorkoutPre
     return await Preferences.create(workoutPreference);
 }
 
+export const getWorkoutPreference = async (userId: number) => {
+    return await Preferences.findAll({ where: { user_id: userId } });
+}
+
+export const updateWorkoutPreference = async (workoutPreference: WorkoutPreferenceType) => {
+    return await Preferences.update(workoutPreference, { where: { preference_id: workoutPreference.preference_id } });
+}
+
+export const deleteWorkoutPreference = async (preferenceId: number) => {
+    return await Preferences.destroy({ where: { preference_id: preferenceId } });
+}
+
+
