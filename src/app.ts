a822e19373a3a10850fa672cff1dc7c110fb698f
Prince Isaac Pantino <arispantino@gmail.com>
2024-11-24 17:15:28 +0800

    feat: added workout preference route

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,7 @@ import { testSequelize } from './config/database';
 import { setupAssociations } from './models';
 import authRoutes from './routes/auth.routes'
 import userRoutes from './routes/user.routes';
+import workoutPreferenceRoutes from './routes/workout-preference.routes';
 const app: Express = express();
 
 app.use(express.json());
@@ -13,6 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 // routes
 app.use('/auth', authRoutes)
 app.use('/user', userRoutes)
+app.use('/work-preference', workoutPreferenceRoutes)
 
 // database init and server start
 async function startServer(): Promise<void> {
