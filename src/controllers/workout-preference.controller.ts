2a92be1e7bc6a49102fa95115e1bc11a5f4898e6
Prince Isaac Pantino <arispantino@gmail.com>
2024-11-24 14:04:56 +0800

    feat: added user id check

--- src/controllers/workout-preference.controller.ts
+++ src/controllers/workout-preference.controller.ts
@@ -1,9 +1,17 @@
 import { Request, Response } from "express";
 import { create, get, update, remove } from "../services/workout-preference.service";
-
+import { WorkoutPreferenceType } from "../types/types";
 export const createWorkoutPreference = async (req: Request, res: Response) => {
     try{
-        const workoutPreference = req.body;
+        if (!req.user?.id) {
+            return res.status(401).json({ error: 'User not authenticated' });
+        }
+
+        const workoutPreference: WorkoutPreferenceType = {
+            ...req.body,
+            user_id: req.user.id
+        };
+
         const newWorkoutPreference = await create(workoutPreference);
         res.status(201).json(newWorkoutPreference);
     }catch(error){
