b6815731a923e8347a095ec904970852f8e0cd4e
Prince Isaac Pantino <arispantino@gmail.com>
2024-11-24 17:51:22 +0800

    feat: made plan exercise a main table and is directly under workout plans

--- src/models/plan-exercise.model.ts
+++ src/models/plan-exercise.model.ts
@@ -2,12 +2,26 @@ import { DataTypes } from "sequelize";
 import sequelize from "../config/database";
 
 const PlanExercise = sequelize.define('PlanExercise', {
+    plan_exercise_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+    },
     plan_id: {
         type: DataTypes.INTEGER,
         allowNull: false
     },
-    exercise_id: {
-        type: DataTypes.INTEGER,
+    exercise_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    description: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    duration_mins: {
+        type: DataTypes.FLOAT,
         allowNull: false
     },
     workout_day: {
@@ -23,6 +37,7 @@ const PlanExercise = sequelize.define('PlanExercise', {
         allowNull: false
     }
 }, {
+    tableName: 'plan_exercises',
     underscored: true,
     timestamps: true
 });
