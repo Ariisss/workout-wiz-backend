b7c50a15ff993661412c392de85dfdddf23d165e
Prince Isaac Pantino <arispantino@gmail.com>
2024-11-24 21:32:37 +0800

    feat: added workout schema for type safety on generation

--- src/config/ai.config.ts
+++ src/config/ai.config.ts
@@ -3,5 +3,75 @@ import env from "./environment";
 
 const genAI = new GoogleGenerativeAI(env.GEMINI_API_KEY);
 
+export const workoutPlanSchema = {
+    description: "Workout Plan",
+    type: SchemaType.ARRAY,
+    items: {
+        type: SchemaType.OBJECT,
+        properties: {
+            "Plan Name": {
+                type: SchemaType.STRING,
+                description: "Name of the workout plan",
+                nullable: false,
+            },
+            Description: {
+                type: SchemaType.STRING,
+                description: "Description of the workout plan",
+                nullable: false,
+            },
+            Goal: {
+                type: SchemaType.STRING,
+                description: "Goal of the workout plan",
+                nullable: false,
+            },
+            Duration_Weeks: {
+                type: SchemaType.NUMBER,
+                description: "Duration of the workout plan in weeks",
+                nullable: false,
+            },
+            Intensity: {
+                type: SchemaType.STRING,
+                description: "Intensity of the workout plan",
+                nullable: false,
+            },
+            Exercises: {
+                type: SchemaType.ARRAY,
+                description: "List of exercises in the workout plan",
+                items: {
+                    type: SchemaType.OBJECT,
+                    properties: {
+                        "Exercise Name": {
+                            type: SchemaType.STRING,
+                            description: "Name of the exercise",
+                            nullable: false,
+                        },
+                        Description: {
+                            type: SchemaType.STRING,
+                            description: "Description of how to perform the exercise",
+                            nullable: false,
+                        },
+                        Sets: {
+                            type: SchemaType.NUMBER,
+                            description: "Number of sets",
+                            nullable: false,
+                        },
+                        Reps: {
+                            type: SchemaType.NUMBER,
+                            description: "Number of repetitions per set",
+                            nullable: false,
+                        },
+                        "Workout Day": {
+                            type: SchemaType.STRING,
+                            description: "Day of the week to perform the workout",
+                            nullable: false,
+                        }
+                    }
+                }
+            }
+        }
+    }
+};
+
+
 export default genAI;
 
