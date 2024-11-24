162eb489ce1232d4f74f7bf2cc280068e6ef12a0
Prince Isaac Pantino <arispantino@gmail.com>
2024-11-24 17:16:24 +0800

    fix: void return instead of response

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -16,7 +16,8 @@ export function authMiddleware(req: Request, res: Response, next: NextFunction) 
   const token = req.headers.authorization?.split(' ')[1];
 
   if (!token) {
-    return res.status(401).json({ message: 'No token provided' });
+    res.status(401).json({ message: 'No token provided' });
+    return
   }
 
   try {
