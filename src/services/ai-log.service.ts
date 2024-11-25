97e3c6c9cc5e170016523c48b45625c5e5a64732
Prince Isaac Pantino <arispantino@gmail.com>
2024-11-26 00:14:19 +0800

    feat: removed 2 unused functions

--- src/services/ai-log.service.ts
+++ src/services/ai-log.service.ts
@@ -7,11 +7,3 @@ export const createAILog = async (user_id: number, prompt: string, response: str
 export const getAILogs = async (user_id: number) => {
     return await AIGenerationLog.findAll({ where: { user_id } });
 }
-
-export const updateAILog = async (id: number, user_id: number, prompt: string, response: string, status: string) => {
-    return await AIGenerationLog.update({ user_id, prompt, response, status }, { where: { ai_log_id: id } });
-}
-
-export const deleteAILog = async (id: number) => {
-    return await AIGenerationLog.destroy({ where: { ai_log_id: id } });
-}
