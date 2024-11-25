95f3d9f13d5515e1bfad938a9ace5f6a01d5cc1f
Prince Isaac Pantino <arispantino@gmail.com>
2024-11-26 00:12:19 +0800

    feat: added proper parameters and removed get ai log by id

--- src/services/ai-log.service.ts
+++ src/services/ai-log.service.ts
@@ -1,21 +1,17 @@
 import AIGenerationLog from "../models/ai-log.model";
 
-export const createAILog = async (data: any) => {
-    return await AIGenerationLog.create(data);
+export const createAILog = async (user_id: number, prompt: string, response: string, status: string) => {
+    return await AIGenerationLog.create({ user_id, prompt, response, status });
 }
 
 export const getAILogs = async (user_id: number) => {
     return await AIGenerationLog.findAll({ where: { user_id } });
 }
 
-export const getAILogById = async (id: number) => {
-    return await AIGenerationLog.findByPk(id);
-}
-
-export const updateAILog = async (id: number, data: any) => {
-    return await AIGenerationLog.update(data, { where: { id } });
+export const updateAILog = async (id: number, user_id: number, prompt: string, response: string, status: string) => {
+    return await AIGenerationLog.update({ user_id, prompt, response, status }, { where: { ai_log_id: id } });
 }
 
 export const deleteAILog = async (id: number) => {
-    return await AIGenerationLog.destroy({ where: { id } });
+    return await AIGenerationLog.destroy({ where: { ai_log_id: id } });
 }
