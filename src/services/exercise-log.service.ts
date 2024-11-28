4b863c597bc060a379c83ab8a61e686ff44377c3
Prince Isaac Pantino <arispantino@gmail.com>
2024-11-28 15:48:43 +0800

    feat: created exercise log service functions

--- src/services/exercise-log.service.ts
+++ src/services/exercise-log.service.ts
@@ -5,4 +5,18 @@ export const createExerciseLog = async (exerciseLog: Omit<ExerciseLogType, 'exer
     return await ExerciseLog.create(exerciseLog);
 }
 
+export const getExerciseLogs = async (user_id: number) => {
+    return await ExerciseLog.findAll({ where: { user_id } });
+}
 
+export const getExerciseLogById = async (log_id: number) => {
+    return await ExerciseLog.findByPk(log_id);
+}
+
+export const updateExerciseLog = async (log_id: number, exerciseLog: Omit<ExerciseLogType, 'log_id'>) => {
+    return await ExerciseLog.update(exerciseLog, { where: { log_id } });
+}
+
+export const deleteExerciseLog = async (log_id: number) => {
+    return await ExerciseLog.destroy({ where: { log_id } });
+}
