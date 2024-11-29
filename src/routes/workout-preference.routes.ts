98f800d94f1376d873878a9dddeb2b9a3c7c2991
Prince Isaac Pantino <arispantino@gmail.com>
2024-11-29 13:17:49 +0800

    feat: added get workout preferences route

--- src/routes/workout-preference.routes.ts
+++ src/routes/workout-preference.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { createWorkoutPreference } from "../controllers/workout-preference.controller";
+import { createWorkoutPreference, getWorkoutPreferences } from "../controllers/workout-preference.controller";
 import { authMiddleware } from "../middleware/auth.middleware";
 
 const router = Router();
 
 router.post('/', authMiddleware, createWorkoutPreference);
+router.get('/', authMiddleware, getWorkoutPreferences);
 
 export default router;
