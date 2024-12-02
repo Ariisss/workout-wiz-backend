1ab4e2b1c5271ee5e39728261f54e68510fb2b27
Prince Isaac Pantino <arispantino@gmail.com>
2024-12-02 19:48:39 +0800

    feat: added delete plan endpoint

--- src/routes/workout-plan.routes.ts
+++ src/routes/workout-plan.routes.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { authMiddleware } from "../middleware/auth.middleware";
-import { generateFromPreferences, createWorkoutPlan, getWorkoutPlan } from "../controllers/workout-plan.controller";
+import { generateFromPreferences, createWorkoutPlan, getWorkoutPlan, deleteWorkoutPlan } from "../controllers/workout-plan.controller";
 
 const router = Router();
 
 router.post('/generate/preferences', authMiddleware, generateFromPreferences);
 router.post('/generate', authMiddleware, createWorkoutPlan);
 router.get('/:id', authMiddleware, getWorkoutPlan);
+router.delete('/:id', authMiddleware, deleteWorkoutPlan);
 
 export default router;
 
