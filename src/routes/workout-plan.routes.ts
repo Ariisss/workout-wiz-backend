f5229a33a70894386aac83667c8415488b1ab1ef
Prince Isaac Pantino <arispantino@gmail.com>
2024-12-03 23:51:06 +0800

    feat: add route for retrieving workout plan exercises to workout plan routes

--- src/routes/workout-plan.routes.ts
+++ src/routes/workout-plan.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { authMiddleware } from "../middleware/auth.middleware";
-import { generateFromPreferences, createWorkoutPlan, getWorkoutPlan, deleteWorkoutPlan } from "../controllers/workout-plan.controller";
+import { generateFromPreferences, createWorkoutPlan, getWorkoutPlan, deleteWorkoutPlan, getWorkoutPlanExercises } from "../controllers/workout-plan.controller";
 
 const router = Router();
 
@@ -8,6 +8,7 @@ router.post('/generate/preferences', authMiddleware, generateFromPreferences);
 router.post('/generate', authMiddleware, createWorkoutPlan);
 router.get('/:id', authMiddleware, getWorkoutPlan);
 router.delete('/:id', authMiddleware, deleteWorkoutPlan);
+router.get('/', authMiddleware, getWorkoutPlanExercises);
 
 export default router;
 
