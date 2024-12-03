f718b4e4aed2f09923a46712cd66617fb219a812
Prince Isaac Pantino <arispantino@gmail.com>
2024-12-04 00:43:08 +0800

    feat: update generateFromPreferences to retrieve user preferences and generate workout plans accordingly

--- src/controllers/workout-plan.controller.ts
+++ src/controllers/workout-plan.controller.ts
@@ -1,11 +1,35 @@
 import { Request, Response } from "express";
 import { getPlanById, deletePlan, getAllPlans, getPlansAndExercises } from "../services/workout-plan.service";
 import { generateWorkoutPlans } from "../services/ai.service";
+import { getPreference } from "../services/workout-preference.service";
+import { WorkoutPreferenceType } from "../types/types";
+import { GoalType, IntensityLevel } from "../types/workout-types";
 
 export const generateFromPreferences = async (req: Request, res: Response) => {
     try {
-        const { preferences } = req.body;
-        const workoutPlan = await generateWorkoutPlans(preferences);
+        const preferences = await getPreference(req.user.id);
+        
+        if (!preferences || preferences.length === 0) {
+            res.status(404).json({
+                error: 'No preferences found for this user'
+            });
+            return;
+        }
+
+        console.log("Preferences: ", preferences);
+
+        const [currentPreference] = preferences;
+        
+        const workoutPreference: WorkoutPreferenceType = {
+            preference_id: currentPreference.get('preference_id') as number,
+            user_id: currentPreference.get('user_id') as number,
+            goal_type: currentPreference.get('goal_type') as GoalType,
+            with_gym: currentPreference.get('with_gym') as boolean,
+            workout_days: currentPreference.get('workout_days') as string,
+            intensity: currentPreference.get('intensity') as IntensityLevel
+        };
+
+        const workoutPlan = await generateWorkoutPlans(workoutPreference);
         res.status(200).json({
             success: true,
             data: workoutPlan
