9d44fa0b5019f885e31564b488642f1ac731daa9
Prince Isaac Pantino <arispantino@gmail.com>
2024-12-03 09:57:27 +0800

    refactor: rename exercise log service methods for consistency and clarity

--- src/services/exercise-log.service.ts
+++ src/services/exercise-log.service.ts
@@ -2,23 +2,23 @@ import { ExerciseLog } from "../models";
 import { ExerciseLogType } from "../types/types";
 import { getPlanExerciseById } from "./exercises.service";
 
-export const createExerciseLog = async (exerciseLog: Omit<ExerciseLogType, 'exercise_log_id'>, caloriesBurned: number) => {
+export const createLog = async (exerciseLog: Omit<ExerciseLogType, 'exercise_log_id'>, caloriesBurned: number) => {
     return await ExerciseLog.create({ ...exerciseLog, calories_burned: caloriesBurned });
 }
 
-export const getExerciseLogs = async (user_id: number) => {
+export const getLogs = async (user_id: number) => {
     return await ExerciseLog.findAll({ where: { user_id } });
 }
 
-export const getExerciseLogById = async (log_id: number) => {
+export const getLogById = async (log_id: number) => {
     return await ExerciseLog.findByPk(log_id);
 }
 
-export const updateExerciseLog = async (log_id: number, exerciseLog: Omit<ExerciseLogType, 'log_id'>) => {
+export const updateLog = async (log_id: number, exerciseLog: Omit<ExerciseLogType, 'log_id'>) => {
     return await ExerciseLog.update(exerciseLog, { where: { log_id } });
 }
 
-export const deleteExerciseLog = async (log_id: number) => {
+export const deleteLog = async (log_id: number) => {
     return await ExerciseLog.destroy({ where: { log_id } });
 }
 
