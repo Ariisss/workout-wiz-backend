c4f2548d420a124711c3f5cac3eaaa6562b86772
Prince Isaac Pantino <arispantino@gmail.com>
2024-12-04 14:59:04 +0800

    refactor: rename and enhance exercise retrieval functions to support user-specific queries and improve clarity

--- src/services/exercises.service.ts
+++ src/services/exercises.service.ts
@@ -1,11 +1,22 @@
-import { PlanExercise } from "../models";
+import { PlanExercise, WorkoutPlan } from "../models";
 import { PlanExerciseType } from "../types/types";
 
 export const createPlanExercise = async (planExercise: Omit<PlanExerciseType, 'plan_exercise_id'>) => {
     return await PlanExercise.create(planExercise);
 }
 
-export const getPlanExercises = async (plan_id: number) => {
+export const getAllUserExercises = async (user_id: number) => {
+    return await PlanExercise.findAll({
+        include: [{
+            model: WorkoutPlan,
+            as: 'workoutPlan',
+            where: { user_id },
+            attributes: []
+        }]
+    });
+}
+
+export const getSinglePlanExercises = async (plan_id: number) => {
     return await PlanExercise.findAll({ where: { plan_id } });
 }
 
