fc773a1ed78a74d2ef294094be30a3d5f8985eba
Prince Isaac Pantino <arispantino@gmail.com>
2024-12-17 21:13:44 +0800

    refactor: update route parameter names for consistency and uncomment AuthenticatedRequest interface

--- src/routes/exercise-log.routes.ts
+++ src/routes/exercise-log.routes.ts
@@ -6,8 +6,7 @@ const router = Router();
 
 router.post('/', authMiddleware, logExercise);
 router.get('/', authMiddleware, getExerciseLogs);
-router.get('/:id', authMiddleware, getExerciseLogById);
-router.delete('/:id', authMiddleware, deleteExerciseLog);
+router.get('/:log_id', authMiddleware, getExerciseLogById);
+router.delete('/:log_id', authMiddleware, deleteExerciseLog);
 
 export default router;
-
--- src/types/types.ts
+++ src/types/types.ts
@@ -38,9 +38,9 @@ export interface UserLoginType {
   password: string;
 }
 
-// export interface AuthenticatedRequest extends Request {
-//   user: JwtPayload;
-// } 
+export interface AuthenticatedRequest extends Request {
+  user: JwtPayload;
+} 
 
 // workout preference types
 export interface WorkoutPreferenceType {
